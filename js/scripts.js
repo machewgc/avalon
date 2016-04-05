9606d37cde9001a11c10cb788e8b05c523907192
machewgc <mattygcz@gmail.com>
2016-04-04 22:43:14 -0500

    added jQuery click functions on the rulebook page to toggle content under headings

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $("#hidden1").click(function() {
+    $("#paragraph-hidden1").toggle();
+    $(".table-hidden").toggle();
+  });
+  $("#hidden2").click(function() {
+    $("#paragraph-hidden2").toggle();
+    $(".table-hidden").toggle();
+  });
+  $("#hidden3").click(function() {
+    $("#paragraph-hidden3").toggle();
+    $(".table-hidden").toggle();
+  });
+  $("#hidden4").click(function() {
+    $("#paragraph-hidden4").toggle();
+    $(".table-hidden").toggle();
+  });
+  $("#hidden5").click(function() {
+    $("#paragraph-hidden5").toggle();
+    $(".table-hidden").toggle();
+  });
+  $("#hidden6").click(function() {
+    $("#paragraph-hidden6").toggle();
+    $(".table-hidden").toggle();
+  });
+  $("#hidden7").click(function() {
+    $("#paragraph-hidden7").toggle();
+    $(".table-hidden").toggle();
+  });
+})
